e50a8237726de64026542c858c8d05c1704d0aaf
Mukul Rana <mukul.rana@nagarro.com>
2023-09-25 09:39:17 +0530

    first commit

---
+++ FirstCode.java
@@ -0,0 +1,12 @@
+class myClass{
+
+   public static void main(String args[])  //static method  
+    {  
+        Add();
+    }  
+
+    public static void Add(){
+        System.out.println(2+2);
+    }
+
+}
