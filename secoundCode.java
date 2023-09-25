f5fd63375b701b517f945afddadf50d56ce435ac
Mukul Rana <mukul.rana@nagarro.com>
2023-09-25 10:25:49 +0530

    SecoundCode First Commit

---
+++ secoundCode.java
@@ -0,0 +1,12 @@
+class myOneClass{
+
+   public static void main(String args[])  //static method  
+    {  
+        Sub();
+    }  
+
+    public static void Sub(){
+        System.out.println(4-2);
+    }
+
+}
